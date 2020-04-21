cd89d1c515768dd615cd1d6d1e3fae5337044732
neil4droid <cterse@gmail.com>
2020-04-21 23:36:44 +0530

    Add timer in seconds

--- popup/popup.js
+++ popup/popup.js
@@ -1,10 +1,16 @@
 chrome.storage.sync.get("probs_list", function(result){
     if (result && result.probs_list) {
-        str = "Total problems monitored: " + result.probs_list.length + "<br/>";
-        for (let i=0; i < result.probs_list.length; i++) {
-            str += result.probs_list[i].code + ":" + result.probs_list[i].name + "<br/>";
+        str = "Total problems monitored: " + result.probs_list.length + "<br/><br/>";
+        setInterval(updatePopup, 1000);
+
+        function updatePopup() {
+            pstr = "";
+            for (let i=0; i < result.probs_list.length; i++) {
+                pstr += result.probs_list[i].code + " : " + result.probs_list[i].name + " : ";
+                pstr += ((Date.now() - result.probs_list[i].init_ts)/1000) + "<br />";
+            }
+            document.getElementById("main").innerHTML = str + pstr;
         }
-        document.getElementById("main").innerHTML = str;
     }
 
     console.dir(result);
