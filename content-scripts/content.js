2a8648bf746b7f744dbecc8fd071b52604943ccc
neil4droid <cterse@gmail.com>
2020-04-25 17:31:07 +0530

    Change the problem object structure and add problem sessions.
    Popup is not working. Need to modify popup to integrate the changed
    problems object.

--- content-scripts/content.js
+++ content-scripts/content.js
@@ -1,27 +1,50 @@
 window.onload = function() {
     let pageInitInterval = setInterval(setupMonitoringScript, 100);
-    
+    let prob_obj = null;
+
     function setupMonitoringScript() {
         if (document.querySelector("div[data-cy=question-title]")) {
             clearInterval(pageInitInterval);
+            
+            prob_obj = createProblemObject(new Date().getTime());
 
-            prob_obj = getProblemObject(new Date().getTime());
             chrome.storage.sync.get("problem_collection_obj", function(result){
                 if (!result) {
                     console.error("lc-timer:setupMonitoringScript: Error getting result from sync.");
                     return null;
                 }
                 
-                problemsCollectionObject = {};
+                problemsCollection = {};
                 if (result.problem_collection_obj) {
-                    console.debug("lc-timer:setupMonitoringScript: Retrieving existing problem list from storage.");
-                    problemsCollectionObject = result.problem_collection_obj;
+                    problemsCollection = result.problem_collection_obj;
                 }
-                problemsCollectionObject[prob_obj.code] = prob_obj;
 
-                chrome.storage.sync.set({"problem_collection_obj":  problemsCollectionObject}, function(){
-                    console.debug("lc-timer:setupMonitoringScript: set data to storage.")
-                });
+                /*
+                    If problem does not exist in storage, add the problem object. 
+                    If problem exists in storage and
+                        status = active, do not update the problem object in the storage.
+                        status = complete, add a new session to the problem object.
+                */
+                let storageUpdated = false;
+                if (!problemsCollection[prob_obj.code]) {
+                    console.debug("lc-timer:content: Problem does not exist in storage, adding it.");
+                    problemsCollection[prob_obj.code] = prob_obj;
+                    storageUpdated = true;
+                } else {
+                    if (getProblemStatus(problemsCollection[prob_obj.code]) == PROBLEM_STATUS_COMPLETE) {
+                        console.debug("lc-timer:content: Completed problem exists, adding a new session.");
+                        startNewSessionForProblem(problemsCollection[prob_obj.code]);
+                        storageUpdated = true;
+                    } else {
+                        console.debug("lc-timer:content: Problem already active.");
+                    }
+                }
+                
+                if(storageUpdated)
+                    chrome.storage.sync.set({"problem_collection_obj":  problemsCollection}, function(){
+                        console.debug("lc-timer:setupMonitoringScript: set data to storage.");
+                        console.dir(problemsCollection);
+                    });
             });
         }
     }
@@ -39,15 +62,15 @@ window.onload = function() {
                     if (document.getElementsByClassName("success__3Ai7").length > 0) {
                         clearInterval(submissionProcessingInterval);
         
-                        //add end timestamp to problem in storage
+                        //add end timestamp to problem's latest session in storage
                         console.debug("lc-timer:setupSubmitScript: Submission Success. Adding end timestamp.");
                         chrome.storage.sync.get("problem_collection_obj", function(result) {
                             if (!result || !result.problem_collection_obj) {
                                 console.error("lc-timer:setupSubmitScript: Error retrieving problems from storage.");
                                 return null;
                             }
-
-                            result.problem_collection_obj[getProblemObject().code].end_ts = Date.now();
+                            
+                            result.problem_collection_obj[prob_obj.code] = completeActiveProblem(result.problem_collection_obj[prob_obj.code]);
                             
                             chrome.storage.sync.set({"problem_collection_obj": result.problem_collection_obj}, function () {
                                 console.debug("lc-timer:setupSubmitScript: Added end timestamp to problem.");
--- content-scripts/problem_utils.js
+++ content-scripts/problem_utils.js
@@ -1,18 +1,80 @@
-function getProblemObject(init_timestamp) {
-    problemTitleNode = document.querySelector("div[data-cy=question-title]");
+let PROBLEM_TITLE_SELECTOR = "div[data-cy=question-title]";
+let PROBLEM_STATUS_ACTIVE = "active";
+let PROBLEM_STATUS_COMPLETE = "complete";
+let SESSION_STATUS_ACTIVE = PROBLEM_STATUS_ACTIVE;
+let SESSION_STATUS_COMPLETE = PROBLEM_STATUS_COMPLETE;
 
-    probObj = {code: null, name: null, init_ts: init_timestamp};
+function createProblemObject(init_timestamp) {
+    probObj = {code: null, name: null, status: null, sessions_list: []};
 
-    if(!problemTitleNode){
-        console.error("Error getting problem title node.");
+    if ($( PROBLEM_TITLE_SELECTOR ).length){
+        probObj.code = extractProblemCode();
+        probObj.name = extractProblemName();
+        probObj = startNewSessionForProblem(probObj); // also updates the problem status 
     } else {
-        probObj.code = problemTitleNode.innerText.split('.')[0].trim();
-        probObj.name = problemTitleNode.innerText.split('.')[1].trim();
+        console.error("lc-timer:problem_utils: Error getting problem title node.");
     }
 
     return probObj;
 }
 
-function getProblemName() {
-    return document.getElementsByTagName("title")[0].innerText;
+function startNewSessionForProblem(problem) {
+    let newSession = createNewSession(problem, Date.now());
+    problem.sessions_list.push(newSession);
+    problem = setProblemStatus(problem, newSession.s_status);
+
+    return problem;
+}
+
+function createNewSession(problem, init_timestamp) {
+    let session = {s_id: null, s_init_ts: null, s_status: null, s_end_ts: null};
+
+    session.s_id = problem.code + "-" + (problem.sessions_list.length+1);
+    session.s_init_ts = init_timestamp ? init_timestamp : Date.now();
+    session.s_status = SESSION_STATUS_ACTIVE;
+
+    return session;
+}
+
+function getProblemStatus(problem) {
+    return problem.status;
+}
+
+function setProblemStatus(problem, status) {
+    if (!problem) {
+        console.debug("lc-timer:problem_utils.setproblemStatus : faulty arguments");
+    }
+    problem.status = status;
+    return problem; 
+}
+
+function isProblemActive(problem) {
+    return getProblemStatus(problem) == PROBLEM_STATUS_ACTIVE;
+}
+
+function isProblemComplete(problem) {
+    return getProblemStatus(problem) == PROBLEM_STATUS_COMPLETE;
+}
+
+function extractProblemCode() {
+    return $( PROBLEM_TITLE_SELECTOR ).text().split('.')[0].trim();
+}
+
+function extractProblemName() {
+    return $( PROBLEM_TITLE_SELECTOR ).text().split('.')[1].trim();
+}
+
+function completeActiveProblem(problem) {
+    if (!problem || isProblemComplete(problem)) {
+        console.debug("lc-timer:problem_utils.completeActiveProblem : Faulty arguemnt");
+        return null;
+    }
+
+    let latestProblemSession = problem.sessions_list[problem.sessions_list.length-1];
+    latestProblemSession.s_end_ts = Date.now();
+    latestProblemSession.s_status = SESSION_STATUS_COMPLETE;
+
+    problem = setProblemStatus(problem, latestProblemSession.s_status);
+
+    return problem;
 }
