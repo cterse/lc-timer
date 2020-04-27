3f5133f1ad35fa60de4a42bc40bc297eea5d3838
neil4droid <cterse@gmail.com>
2020-04-28 00:24:00 +0530

    Create function to generate popup content dynamically

--- popup/popup.js
+++ popup/popup.js
@@ -1,3 +1,11 @@
+// $( document ).ready(function() {
+//     for (var i=0; i<5; i++) {
+//         $('#main-container')
+//             .append('<div id="new-row-'+i+'" class="row my-row">');
+//         $('#new-row-'+i).append('<p>Test: '+i+'</p>');
+//     }
+// });
+
 chrome.storage.sync.get([constants.STORAGE_PROBLEM_COLLECTION], function(result){
     if(!result) {
         console.error("popup: Error retrieving result from storage.");
@@ -18,9 +26,29 @@ chrome.storage.sync.get([constants.STORAGE_PROBLEM_COLLECTION], function(result)
                     latestSessionEnd = latestSessionEnd ? latestSessionEnd : Date.now();
                     timeElapsed = latestSessionEnd - latestSessionStart;
 
-                    pstr += problemObj.code + " : " + problemObj.name + " : ";
-                    pstr += getTimerString(timeElapsed);
-                    pstr += "<br />";
+                    // pstr += problemObj.code + " : " + problemObj.name + " : ";
+                    // pstr += getTimerString(timeElapsed);
+                    // pstr += "<br />";
+                    
+                    $('#main-container').empty();
+
+                    $('#main-container').append('<div class="row"> id="problem-'+problemObj.code+'"');
+                    $('#problem-'+problemObj.code).append('<div class="col-2-auto">'+problemObj.code+' -&nbsp;</div>');
+                    $('#problem-'+problemObj.code).append('<div class="col-3-auto">'+problemObj.name+'</div>');
+                    $('#problem-'+problemObj.code).append('<div class="col-3-auto ml-auto">'+getTimerString(timeElapsed)+'&nbsp;</div>');
+                    $('#problem-'+problemObj.code).append('<div class="col-1-auto"><a data-toggle="collapse" href="#sessionsDiv-'+problemObj.code+'" role="button" aria-expanded="false" aria-controls="sessionsDiv-'+problemObj.code+'" data-placement="bottom" title="Previous Sessions"><svg class="bi bi-chevron-down" width="1em" height="1em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M1.646 4.646a.5.5 0 01.708 0L8 10.293l5.646-5.647a.5.5 0 01.708.708l-6 6a.5.5 0 01-.708 0l-6-6a.5.5 0 010-.708z" clip-rule="evenodd"/></svg></a></div>');
+
+                    $('#main-container').append('<div class="collapse" id="sessionsDiv-'+problemObj.code+'">');
+                    for (var s_it=problemObj.sessions_list.length-1; s_it >= 0; s_it--) {   // s_it = session_iterator
+                        let session = problemObj.sessions_list[s_it];
+                        let sessionRowId = problemObj.code + "-" + session.s_id;
+                        let sessionTime = getTimerString(session.s_end_ts - session.s_init_ts);
+
+                        $('#sessionsDiv-'+problemObj.code).append('<div class="row justify-content-end"> id="'+sessionRowId+'"');
+                        $('#'+sessionRowId).append('<div class="col-auto">'+session.s_id+'</div>');
+                        $('#'+sessionRowId).append('<div class="col-auto">'+sessionTime+'</div>');
+                        $('#'+sessionRowId).append('<div class="col-auto">Session end timestamp</div>');
+                    }
                 }
             }
             document.getElementById("main").innerHTML = pstr;
