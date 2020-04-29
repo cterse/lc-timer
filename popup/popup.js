6bad4ec2d9468b54bbee11aa04cde716090cd3f8
neil4droid <cterse@gmail.com>
2020-04-29 23:49:37 +0530

    Fixed timer for multiple simultaneous active problems

--- popup/popup.js
+++ popup/popup.js
@@ -21,16 +21,18 @@ chrome.storage.sync.get([constants.STORAGE_PROBLEM_COLLECTION], function(result)
                     $('#problem-'+problemCode).append('<div class="col-3-auto ml-auto" id="problem-'+problemCode+'-timer"></div>');
                     $('#problem-'+problemCode).append('<div class="col-1-auto"><a data-toggle="collapse" href="#sessionsDiv-'+problemCode+'" role="button" aria-expanded="false" aria-controls="sessionsDiv-'+problemCode+'" data-placement="bottom" title="Previous Sessions"><svg class="bi bi-chevron-down" width="1em" height="1em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M1.646 4.646a.5.5 0 01.708 0L8 10.293l5.646-5.647a.5.5 0 01.708.708l-6 6a.5.5 0 01-.708 0l-6-6a.5.5 0 010-.708z" clip-rule="evenodd"/></svg></a></div>');
 
-                    setInterval(updateTimestampString, 1000);
-                    function updateTimestampString() {
-                        latestSessionStart = problemObj.sessions_list[problemObj.sessions_list.length-1].s_init_ts;
-                        latestSessionEnd = problemObj.sessions_list[problemObj.sessions_list.length-1].s_end_ts;
-                        latestSessionEnd = latestSessionEnd ? latestSessionEnd : Date.now();
-                        timeElapsed = latestSessionEnd - latestSessionStart;
-
-                        $('#problem-'+problemCode+'-timer').text(getTimerString(timeElapsed));
-                    }
-
+                    (function(problem) {
+                        setInterval(updateTimestampString, 1000);
+                        function updateTimestampString() {
+                            let latestSessionStart = problem.sessions_list[problem.sessions_list.length-1].s_init_ts;
+                            let latestSessionEnd = problem.sessions_list[problem.sessions_list.length-1].s_end_ts;
+                            latestSessionEnd = latestSessionEnd ? latestSessionEnd : Date.now();
+                            let timeElapsed = latestSessionEnd - latestSessionStart;
+                            
+                            $('#problem-'+problem.code+'-timer').text(getTimerString(timeElapsed));
+                        }
+                    })(problemObj);
+                    
                     $('#main-container').append('<div class="collapse sessions-div" id="sessionsDiv-'+problemCode+'">');
                     for (var s_it=problemObj.sessions_list.length-1; s_it >= 0; s_it--) {   // s_it = session_iterator
                         let session = problemObj.sessions_list[s_it];
