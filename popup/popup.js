1250c46524b76b15a2c5c829c199f503e46390ea
neil4droid <cterse@gmail.com>
2020-05-02 14:13:32 +0530

    Remove the dropdown image and set dropdown link on timer string

--- popup/popup.js
+++ popup/popup.js
@@ -51,8 +51,8 @@ function generateProblemRowHTML(problem) {
         $('#problem-'+problemCode+'-name').removeClass("col-5").addClass("col-5-auto").text(problem.name);
     }
 
-    $('#problem-'+problemCode).append('<div class="col-4 ml-auto" id="problem-'+problemCode+'-timer"></div>');
-    $('#problem-'+problemCode).append('<div class="col-1-auto"><a data-toggle="collapse" href="#sessionsDiv-'+problemCode+'" role="button" aria-expanded="false" aria-controls="sessionsDiv-'+problemCode+'" data-placement="bottom" title="Previous Sessions"><svg class="bi bi-chevron-down" width="1em" height="1em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M1.646 4.646a.5.5 0 01.708 0L8 10.293l5.646-5.647a.5.5 0 01.708.708l-6 6a.5.5 0 01-.708 0l-6-6a.5.5 0 010-.708z" clip-rule="evenodd"/></svg></a></div>');
+    //Timer string div
+    $('#problem-'+problemCode).append('<div class="col-4-auto ml-auto" id="problem-'+problemCode+'-timer"><a data-toggle="collapse" href="#sessionsDiv-'+problemCode+'" role="button" aria-expanded="false" aria-controls="sessionsDiv-'+problemCode+'" data-placement="bottom" title="Session Info"></a></div>');
 
     // Generate timer string
     (function(problem) {
@@ -63,9 +63,7 @@ function generateProblemRowHTML(problem) {
             latestSessionEnd = latestSessionEnd ? latestSessionEnd : Date.now();
             let timerString = getTimerString(latestSessionEnd - latestSessionStart);
             
-            //TODO: Set problem timer string. Get a marquee if timer string is greater than enclosing col offsetWidth
-            $('#problem-'+problem.code+'-timer').removeClass("col-4").addClass("col-4-auto").text(timerString);
-            
+            $('#problem-'+problem.code+'-timer a').text(timerString);
         }
     })(problem);
     
